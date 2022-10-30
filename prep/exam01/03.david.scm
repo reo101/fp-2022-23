fc121a884e931bc1312a60b21103f2d845e53568
David-Petrov <david.petrov2000@gmail.com>
2022-10-30 16:55:33 +0200

    feat(prem/exam01): add bonus for `03.david.scm`

--- prep/exam01/03.david.scm
+++ prep/exam01/03.david.scm
@@ -31,7 +31,35 @@
 
 ;; finally, solve the initial problem by first slide-mapping consecutive lists to check majors
 ;; and then folding to check if all results are true (which we want to check)
+(define (is-majored-by? l1 l2)
+    (sublist-op? <= l1 l2))
+
 (define (is-major? ll)
     (foldl (lambda (x y) (and x y)) 
            #t 
-           (slide-pair-map (lambda (l1 l2) (sublist-op? <= l1 l2)) ll)))
+           (slide-pair-map is-majored-by? ll)))
+
+;;;;;;;;;;;;;;;;
+;; BONUS PART ;;
+;;;;;;;;;;;;;;;;
+
+(define (longer l1 l2)
+    (if (>= (length l1) (length l2)) l1 l2))
+
+(define (find-longest-major ll)
+    (letrec 
+        ((find-longest-major-acc 
+            (lambda (ll curr most)
+                (let ((curr-len (length curr))
+                        (most-len (length most))) 
+                        (if (null? ll) 
+                            (longer curr most)
+                            (let ((first (car ll)) 
+                                  (rest  (cdr ll)))
+                                 (if (or (null? curr) 
+                                         (is-majored-by? (car curr) first))
+                                     (find-longest-major-acc 
+                                         rest (cons first curr) most)
+                                     (find-longest-major-acc 
+                                         rest (list first) (longer curr most)))))))))
+        (reverse (find-longest-major-acc ll '() '()))))
